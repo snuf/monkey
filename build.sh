0c37c7d637ecc2ebc08b5efc055d57e843d32da9
Funs <funs@barred.org>
2018-01-30 15:50:48 -0800

    Simple builder for complete docker setup

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+build_copy_rm() {
+  name=$1
+  dockerfile=$2
+  src=$3
+  dst=$4
+
+  echo $name, $dockerfile, $src, $dst, $OPTS
+
+  docker build -t $name -f $dockerfile $OPTS .
+  id=$(docker create $name)
+  docker cp $id:$src $dst
+  docker rm -v $id
+}
+
+OPTS=$@
+
+build_copy_rm monkey_chaos Dockerfile.chaos /monkey/chaos_monkey/dist .
+build_copy_rm monkey_chaos32 Dockerfile.chaos32 /monkey/chaos_monkey/dist .
+docker build -t monkey_island -f Dockerfile.island .
